55c33ec5c11d598edddfd865a9f6181cdea6c7b5
kakoucedricakrou <cedric.akrou@cinetpay.com>
2022-11-10 10:09:03 +0000

    feat(app): Add modules.
    Adding project modules and README.md file.

---
+++ shared/src/test/java/com/cedricakrou/my/blogshared/SharedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cedricakrou.my.blogshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SharedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
